a187d85a902aac25a0b45ca2393adf3316f1a216
Дмитрий Бородин <trybr@MacBook-Pro-Dmitrij.local>
2020-07-21 15:40:48 +0200

    Updates

---
+++ precache-manifest.ee64a6ef714d6713aeb604b43e942f91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0b44e79542127ae833553e9ccf3c18",
+    "url": "/tt-management/index.html"
+  },
+  {
+    "revision": "3809b39f40ca2e1b5068",
+    "url": "/tt-management/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "df711667c749e3fcc1dd",
+    "url": "/tt-management/static/css/main.e77932f8.chunk.css"
+  },
+  {
+    "revision": "3809b39f40ca2e1b5068",
+    "url": "/tt-management/static/js/2.4515fce5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tt-management/static/js/2.4515fce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df711667c749e3fcc1dd",
+    "url": "/tt-management/static/js/main.33c0d5d4.chunk.js"
+  },
+  {
+    "revision": "72c769746f23b59db9a3",
+    "url": "/tt-management/static/js/runtime-main.ff851afd.js"
+  }
+]);
